ec4d3c2f60df87d7b23c80cb992fd6b9ef92b63d
Vikas Kasu <vikaskasu@Kasus-MacBook-Pro-2.local>
2020-04-25 17:48:11 -0400

    upgraded Bootstrap 4 and alignment

--- src/app/summary/summary.component.ts
+++ src/app/summary/summary.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DataRetrieval } from '../shared/services/dataRetrieval.service';
 
 @Component({
     selector:'app-summary',
     templateUrl:'./summary.component.html',
     styleUrls:['./summary.component.css']
 })
-export class Summary{
+export class Summary implements OnInit{
+
+    constructor(private dataRetrieval:DataRetrieval){}
+
+    startYear:string;
+    endYear:string;
+
+    ngOnInit() {
+        this.dataRetrieval.getAllYearsForCustomers().subscribe(response=>{
+            this.startYear = response[0];
+            this.endYear = response[response["length"]-1];
+        });
+    }
 
 }
