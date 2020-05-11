c5f282f90208db6940a8586a9e4103c7e0ca124b
Vikas Kasu <vikaskasu@Kasus-MacBook-Pro-2.local>
2020-05-11 17:26:45 -0400

    Router added to tabs

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { SummaryComponent } from './summary/summary.component';
+import { ExpensesByYearComponent } from './expensesByYear/expensesByYear.component';
+import { LoansComponent } from './loans/loans.component';
+import { RawDataComponent } from './rawData/rawData.component';
+import { NewInputComponent } from './newInput/newInput.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',component:NewInputComponent},
+  {path:'summary',component:SummaryComponent},
+  {path:'expensesByYear',component:ExpensesByYearComponent},
+  {path:'loans',component:LoansComponent},
+  {path:'rawData',component:RawDataComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'BudgetTracker';
+  navLinks = [];
+  activeLink:string;
+
+  constructor(){
+    this.navLinks = [{path:'/',label:'Home'},
+    {path:'summary',label:'Summary'},
+    {path:'expensesByYear',label:'Expenses By Year'},
+    {path:'loans',label:'Loans'},
+    {path:'rawData',label:'Data'}];
+    this.activeLink = this.navLinks[0].path;
+  }
+  
 }
