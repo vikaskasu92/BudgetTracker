da5502297be55b7b9e80fd24558eb4967a085df6
Vikas Kasu <vikaskasu@Kasus-MacBook-Pro-2.local>
2020-05-16 18:57:40 -0400

    hide show years

--- src/app/summary/summary.component.ts
+++ src/app/summary/summary.component.ts
@@ -14,12 +14,16 @@ export class SummaryComponent implements OnInit{
     endYear:string;
     yearsDataRecieved:boolean = false;
     nowCallPieData:boolean =false;
+    noDataForCustomer:boolean = false;
 
     ngOnInit() {
         this.dataRetrieval.getAllYearsForCustomers().subscribe(response=>{
             this.yearsDataRecieved = true;
             this.startYear = response[0];
             this.endYear = response[response["length"]-1];
+            if(this.startYear === undefined || this.endYear === undefined){
+                this.noDataForCustomer = true;
+            }
             this.nowCallPieData = true;
         });
     }
